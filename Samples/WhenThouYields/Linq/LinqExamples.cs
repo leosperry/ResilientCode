5dd62cdf87f96d51f9670ceed6a16c6f892f200d
Leonard Sperry <leonard.sperry@live.com>
2020-11-15 11:30:00 -0800

    Linq examples

--- Samples/WhenThouYields/Linq/LinqExamples.cs
+++ Samples/WhenThouYields/Linq/LinqExamples.cs
@@ -8,10 +8,33 @@ namespace Samples.WhenThouYields.Linq
 {
     class LinqExamples
     {
-        void test()
+        void twocollection()
         {
-            IEnumerable<SomeBusinessObject> ints = null;
-            ints.Concat;
+            List<Book> books = null;
+            List<Category> categories = null;
+
+        var categoriesUsedInBooks =
+            from cId in books.SelectMany(b => b.CategoryIds).Distinct()
+            join c in categories
+            on cId equals c.ID
+            select c;
+
         }
+    }
+
+    class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+    class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public List<int> CategoryIds { get; set; }
+
     }
 }
---
+++ Samples/WhenThouYields/Linq/WhereExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.WhenThouYields.Linq
+{
+    static class WhereExample
+    {
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> collection, Func<T, bool> filter)
+        {
+            foreach (var item in collection)
+            {
+                if (filter(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
