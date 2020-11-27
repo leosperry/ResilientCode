ef2fdf1c4d5b7f7c0b0907690ec8358be4ebd77e
Leonard Sperry <leonard.sperry@live.com>
2020-11-27 14:12:44 -0800

    New is a four letter word

---
+++ Samples/NewIs4LetterWord/BasicContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Samples.NewIs4LetterWord
+{
+public class BasicContainer
+{
+    Dictionary<Type, Func<object>> _resolvers = new Dictionary<Type, Func<object>>();
+    public void Register<T>(Func<T> resolver)
+    {
+        _resolvers[typeof(T)] = () => resolver();
+    }
+
+    public T Resolve<T>()
+    {
+        return (T)_resolvers[typeof(T)]();
+    }
+}
+}
